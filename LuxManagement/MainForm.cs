d7886f72e6680e746becc14384e892ff2f366b8b
JeremyCPE <mohariry.jeremy@gmail.com>
2024-05-21 21:26:42 +0900

    Ajoutez des fichiers projet.

---
+++ LuxManagement/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LuxManagement
+{
+
+
+    public partial class MainForm : Form
+    {
+        private ColorDialog colorDialog;
+        private TrackBar trackBarLuminosity;
+        private Button applyButton;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            colorDialog = new ColorDialog();
+            trackBarLuminosity = new TrackBar();
+            applyButton = new Button();
+
+            trackBarLuminosity.Minimum = 0;
+            trackBarLuminosity.Maximum = 100;
+            trackBarLuminosity.Value = 50;
+
+            applyButton.Text = "Apply";
+            applyButton.Click += ApplyButton_Click;
+
+            Controls.Add(trackBarLuminosity);
+            Controls.Add(applyButton);
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Color selectedColor = colorDialog.Color;
+                float luminosity = trackBarLuminosity.Value / 100f;
+                Color adjustedColor = ChangeColorLuminosity(selectedColor, luminosity);
+
+                ChangeDesktopColor(ColorTranslator.ToHtml(adjustedColor));
+            }
+        }
+
+        public Color ChangeColorLuminosity(Color color, float luminosity)
+        {
+            float r = color.R * luminosity;
+            float g = color.G * luminosity;
+            float b = color.B * luminosity;
+
+            return Color.FromArgb((int)r, (int)g, (int)b);
+        }
+
+        public void ChangeDesktopColor(string colorHex)
+        {
+            // This is just a placeholder. To change the actual desktop color, 
+            // you would typically change the wallpaper or system theme color.
+            // For example, set a solid color wallpaper.
+            NativeMethods.SystemParametersInfo(NativeMethods.SPI_SETDESKWALLPAPER, 0, colorHex, NativeMethods.SPIF_UPDATEINIFILE | NativeMethods.SPIF_SENDCHANGE);
+        }
+    }
+
+
+
+}
---
+++ LuxManagement/NativeMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LuxManagement
+{
+    public static class NativeMethods
+    {
+        public const uint SPI_SETDESKWALLPAPER = 0x0014;
+        public const uint SPI_GETDESKWALLPAPER = 0x0073;
+        public const uint SPI_SETNONCLIENTMETRICS = 0x002A;
+        public const uint SPI_GETNONCLIENTMETRICS = 0x0029;
+        public const uint SPIF_UPDATEINIFILE = 0x01;
+        public const uint SPIF_SENDCHANGE = 0x02;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NONCLIENTMETRICS
+        {
+            public uint cbSize;
+            public int iBorderWidth;
+            public int iScrollWidth;
+            public int iScrollHeight;
+            public int iCaptionWidth;
+            public int iCaptionHeight;
+            public LOGFONT lfCaptionFont;
+            public int iSmCaptionWidth;
+            public int iSmCaptionHeight;
+            public LOGFONT lfSmCaptionFont;
+            public int iMenuWidth;
+            public int iMenuHeight;
+            public LOGFONT lfMenuFont;
+            public LOGFONT lfStatusFont;
+            public LOGFONT lfMessageFont;
+            public int iPaddedBorderWidth;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LOGFONT
+        {
+            public int lfHeight;
+            public int lfWidth;
+            public int lfEscapement;
+            public int lfOrientation;
+            public int lfWeight;
+            public byte lfItalic;
+            public byte lfUnderline;
+            public byte lfStrikeOut;
+            public byte lfCharSet;
+            public byte lfOutPrecision;
+            public byte lfClipPrecision;
+            public byte lfQuality;
+            public byte lfPitchAndFamily;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string lfFaceName;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+    }
+}
